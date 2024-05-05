32132d2ed4201694f7e07a8f8f618f4f8a89e916
ARUN\vic66 <666888vic@gmail.com>
2024-05-05 10:45:23 -0400

    1,Removed non used variables 2, formated the code

--- TeslaP/src/ultrasound.cpp
+++ TeslaP/src/ultrasound.cpp
@@ -5,10 +5,8 @@ const int tringPin = 7; // Trigger Pin of Ultrasonic Sensor
 const int echoPin = 8;  // Echo Pin of Ultrasonic Sensor
 int flexiPower = 0;
 int range;
-int current_range_index = 0;
 int max_range_index = 11;
 int us_measure_timer;
-bool us_ready_for_servo = false;
 
 long microsecondsToInches(long microseconds)
 {
@@ -42,20 +40,6 @@ void us_loop()
     cm = microsecondsToCentimeters(duration);
     if (duration > 0)
     {
-
-        // us_measure_timer = 0;
-
         range = cm;
-        // Serial.print("distance of range ");
-        // // Serial.print(current_range_index);
-        // Serial.print(":");
-        // Serial.print(range);
-        // Serial.println("cm");
-        // current_range_index++;
-        // if (current_range_index == max_range_index)
-        // {
-        //     current_range_index = 0;
-        // }
     }
-    us_ready_for_servo = true;
 }
